2d3dd2a3f776c2a35f7009542304f5bf82da70b4
tom pitoun <tpitorin@myges.fr>
2023-05-28 21:32:21 +0200

    Add WorldListOption

--- veirf2.0.py
+++ veirf2.0.py
@@ -17,13 +17,27 @@ COLOR_YELLOW = colorama.Fore.YELLOW
 COLOR_RED = colorama.Fore.RED
 COLOR_RESET = colorama.Style.RESET_ALL
 
-def rechercher_fichiers(mot_cle, chemin_recherche):
+def rechercher_fichiers_par_mot(mot_cle, chemin_recherche):
     fichiers_trouves = []
+
     for root, _, _ in os.walk(chemin_recherche):
         for chemin_fichier in glob.glob(os.path.join(root, "*")):
             if mot_cle.lower() in os.path.basename(chemin_fichier).lower():
                 fichiers_trouves.append(chemin_fichier)
                 print("Fichier trouvé :", COLOR_GREEN + chemin_fichier + COLOR_RESET)
+
+    return fichiers_trouves
+
+def rechercher_fichiers_par_liste_mots(mots_cles, chemin_recherche):
+    fichiers_trouves = []
+
+    for root, _, _ in os.walk(chemin_recherche):
+        for chemin_fichier in glob.glob(os.path.join(root, "*")):
+            nom_fichier = os.path.basename(chemin_fichier).lower()
+            if any(mot.lower() in nom_fichier for mot in mots_cles):
+                fichiers_trouves.append(chemin_fichier)
+                print("Fichier trouvé :", COLOR_GREEN + chemin_fichier + COLOR_RESET)
+
     return fichiers_trouves
 
 # Afficher les crédits
@@ -31,8 +45,14 @@ print(COLOR_BLUE + "Crédits du développeur :" + COLOR_RESET)
 print("Développé par : " + COLOR_GREEN + "MrAnthony#6281" + COLOR_RESET)
 print("Pour " + COLOR_YELLOW + "Synaria" + COLOR_RESET)
 
-# Obtenir le mot-clé à partir de l'entrée de l'utilisateur
-mot_cle = input("Entrez le mot-clé à rechercher : ")
+# Obtenir l'option de recherche à partir de l'entrée de l'utilisateur
+option_recherche = input("Veuillez choisir une option de recherche : \n1. Rechercher avec un mot\n2. Rechercher avec une liste de mots\n")
+
+# Obtenir le mot-clé ou la liste de mots à partir de l'entrée de l'utilisateur
+if option_recherche == "1":
+    mot_cle = input("Entrez le mot-clé à rechercher : ")
+elif option_recherche == "2":
+    mots_cles = input("Entrez une liste de mots séparés par des espaces : ").split()
 
 # Obtenir le répertoire de recherche à partir de l'entrée de l'utilisateur
 chemin_recherche = input("Entrez le répertoire de recherche : ")
@@ -44,32 +64,39 @@ os.system('cls' if os.name == 'nt' else 'clear')
 print(COLOR_BLUE + "Crédits du développeur :" + COLOR_RESET)
 print("Développé par : " + COLOR_GREEN + "MrAnthony#6281" + COLOR_RESET)
 print("Pour " + COLOR_YELLOW + "Synaria" + COLOR_RESET)
-print("\n Mot recherché: " + mot_cle)
-print("\n Répertoire selectionné: " + chemin_recherche )
+print("\nOption de recherche: " + option_recherche)
+
+if option_recherche == "1":
+    print("\nMot-clé recherché: " + mot_cle)
+elif option_recherche == "2":
+    print("\nListe de mots recherchés: ", mots_cles)
+
+print("\nRépertoire sélectionné: " + chemin_recherche)
 
 # Mesurer le temps d'exécution
 temps_debut = time.time()
 
-# Appeler la fonction rechercher_fichiers
-resultats_recherche = rechercher_fichiers(mot_cle, chemin_recherche)
+# Appeler la fonction appropriée en fonction de l'option de recherche
+if option_recherche == "1":
+    resultats_recherche = rechercher_fichiers_par_mot(mot_cle, chemin_recherche)
+elif option_recherche == "2":
+    resultats_recherche = rechercher_fichiers_par_liste_mots(mots_cles, chemin_recherche)
 
 temps_fin = time.time()
 temps_execution = temps_fin - temps_debut
 
 if resultats_recherche:
-    print("\nLa recherche est terminée")
+    print("\nLa recherche est terminée.")
 else:
     print("\nLa recherche est terminée. Aucun fichier trouvé.")
 
 print(COLOR_YELLOW + "\nTemps d'exécution :", temps_execution, "secondes." + COLOR_RESET)
 
 # Afficher les crédits
-
 print("Développé par : " + COLOR_GREEN + "MrAnthony#6281" + COLOR_RESET)
 print("Pour " + COLOR_YELLOW + "Synaria" + COLOR_RESET)
 
 input("\nAppuyez sur Entrée pour quitter...")
-input("\n" + COLOR_RED + "Appuyez sur Entrée pour confirmer..."+ COLOR_RESET)
 
 # Réinitialiser Colorama
 colorama.deinit()
